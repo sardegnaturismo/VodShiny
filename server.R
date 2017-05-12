4fa34d05e974ad6176ce5c05501ec3331ea5a5dd
Pg <palla.pg@gmail.com>
2017-05-12 09:25:18 +0200

    * add dashboard structure * add folder for css customization

--- server.R
+++ server.R
@@ -6,18 +6,10 @@
 #
 
 library(shiny)
+library(shinydashboard)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
+     
 
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,36 @@
 
 
 library(shiny)
+library(shinydashboard)
+source("dash_header.R")
 
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
+shinyUI(
+        dashboardPage(
+                skin = "red",
+                header <- dash_header(),
+                dashboardSidebar(
+  
+                ),
+                dashboardBody()
 ))
+# shinyUI(fluidPage(
+# 
+#   # Application title
+#   titlePanel("Old Faithful Geyser Data"),
+# 
+#   # Sidebar with a slider input for number of bins
+#   sidebarLayout(
+#     sidebarPanel(
+#       sliderInput("bins",
+#                   "Number of bins:",
+#                   min = 1,
+#                   max = 50,
+#                   value = 30)
+#     ),
+# 
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#       plotOutput("distPlot")
+#     )
+#   )
+# ))
