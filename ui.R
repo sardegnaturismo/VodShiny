6eb80533d7177027749eba773cd14519e71e8aa7
pg <palla.pg@gmail.com>
2017-05-12 11:39:57 +0200

    * change the user interface * add some library and functions

--- ui.R
+++ ui.R
@@ -3,34 +3,16 @@
 library(shiny)
 library(shinydashboard)
 source("dash_header.R")
+source("build_sidebar.R")
+source("build_items.R")
 
 shinyUI(
         dashboardPage(
                 skin = "red",
                 header <- dash_header(),
-                dashboardSidebar(
-  
-                ),
-                dashboardBody()
+                sidebar <- build_sidebar(),
+                dashboardBody(
+                  items <- build_items()
+                )
 ))
-# shinyUI(fluidPage(
-# 
-#   # Application title
-#   titlePanel("Old Faithful Geyser Data"),
-# 
-#   # Sidebar with a slider input for number of bins
-#   sidebarLayout(
-#     sidebarPanel(
-#       sliderInput("bins",
-#                   "Number of bins:",
-#                   min = 1,
-#                   max = 50,
-#                   value = 30)
-#     ),
-# 
-#     # Show a plot of the generated distribution
-#     mainPanel(
-#       plotOutput("distPlot")
-#     )
-#   )
-# ))
+
