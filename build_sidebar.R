cd14a58afa3302ddc9c2551e2052846dd6f7bd11
Pg <palla.pg@gmail.com>
2017-05-23 12:53:44 +0200

    * add menu "Turismo" * add sub menus to "Turismo" * add menu "Turismo interno" * add sub menus to "Turismo interno" * add menu "Co-visite" * add submenus to "Co-visite"

--- build_sidebar.R
+++ build_sidebar.R
@@ -3,10 +3,25 @@ build_sidebar <- function(){
   dashboardSidebar(
     sidebarMenu(id="items",
                 menuItem("home", tabName = "home", icon = icon("home")),
-                menuItem("Overview", tabName = "overview", icon = icon("area-chart")),
-                menuItem("Turismo Interno", tabName = "tinterno", icon = icon("bed"))
-                )
-  )
-  
+                menuItem("Turismo", icon = icon("area-chart"),
+                         menuSubItem("Provenienza visitatori", tabName = "provenienze"),
+                         menuSubItem("Provenienza nel periodo", tabName = "provenienze_tmp"),
+                         menuSubItem("Presenza it e st", tabName = "presenze_it"),
+                         menuSubItem("Presenza nei Comuni", tabName = "com_prov")),
+                         # menuSubItem("Presenza stranieri", tabName = "presenze_st")),
+        
+                menuItem("Turismo Interno", icon = icon("bed"),
+                         menuSubItem("Province", tabName = "prov_interno"),
+                         menuSubItem("Comuni", tabName = "com_interno")
+                         ),
+                
+                menuItem("Co-visite", icon = icon("share-alt-square"),
+                         menuSubItem("Totali e Residenti", tabName = "cov_tot"),
+                         # menuSubItem("Residenti", tabName = "cov_res"),
+                         menuSubItem("Visitatori italiani e stranieri", tabName = "cov_vis")
+                        
+                        )
+                ))
+        
   
 }
