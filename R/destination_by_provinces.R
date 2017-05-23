23a634048ef84d9616d15cc430c7f937cf94503c
Pg <palla.pg@gmail.com>
2017-05-23 16:08:06 +0200

    * add function destination_by_provinces to provide a dataset of visitors of a province as a function of province they come from * add function destiation_by_province_and_time to provide a dataframe of visitors of a province as a function of the province of origin and time * add function get_province_symbols to associate the name of a province with a symbol

---
+++ R/destination_by_provinces.R
@@ -0,0 +1,32 @@
+source("R/constants.R")
+
+destination_by_provinces <- function(dataset, province_name){
+        d <- dataset[dataset$province == province_name, ]
+        res <- aggregate(d$unique_visitors ~ d$origin_province, FUN = sum)
+        names(res) <- c("origin", "visitors")
+        res <- res[order(res$visitors, decreasing = T), ]
+        res
+        
+}
+
+
+destination_by_province_and_time <- function(dataset, province_name){
+        d <- dataset[dataset$province == province_name, ]
+        res <- aggregate(d$unique_visitors ~ d$origin_province + d$day, FUN = sum)
+        names(res) <- c("origin_province", "month", "visitors")
+        res$month = gsub(pattern = " 0:00", replacement = "", x = res$month)
+        res$month <- as.Date(res$month, "%m/%d/%y")
+        res = res[order(res$month, decreasing = F), ]
+}
+
+
+get_province_symbols <- function(province_names){
+        
+        prov_symbols = c()
+        for (p in province_names){
+                symbol = province_symbols[[p]]
+                prov_symbols = append(prov_symbols, symbol)
+        }
+        return(prov_symbols)
+        
+}
