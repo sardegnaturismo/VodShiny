d81690a9097079f8408be59df541a021960c2f8e
pg <palla.pg@gmail.com>
2017-06-01 15:51:26 +0200

    * modify sidebar layout moving up the tab "Co-visite"

--- build_sidebar.R
+++ build_sidebar.R
@@ -9,20 +9,19 @@ build_sidebar <- function(){
                          menuSubItem("Comuni: provenienze", tabName = "com_by_prov"),
                          menuSubItem("Provenienza nel periodo", tabName = "provenienze_tmp"),
                          menuSubItem("Presenza it e st", tabName = "presenze_it")),
-                         
-                         # menuSubItem("Presenza stranieri", tabName = "presenze_st")),
-        
-                menuItem("Turismo Interno", icon = icon("bed"),
-                         menuSubItem("Province", tabName = "prov_interno"),
-                         menuSubItem("Comuni", tabName = "com_interno")
-                         ),
-                
                 menuItem("Co-visite", icon = icon("share-alt-square"),
                          menuSubItem("Totali e Residenti", tabName = "cov_tot"),
                          # menuSubItem("Residenti", tabName = "cov_res"),
                          menuSubItem("Visitatori italiani e stranieri", tabName = "cov_vis")
-                        
-                        )
+                         
+                ),
+                         
+                menuItem("Turismo Interno", icon = icon("bed"),
+                         menuSubItem("Province", tabName = "prov_interno"),
+                         menuSubItem("Comuni", tabName = "com_interno")
+                         )
+                
+
                 ))
         
   
