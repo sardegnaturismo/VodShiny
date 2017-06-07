7b9b607fd2c282b2806d6a013e406de4990b730b
pg <palla.pg@gmail.com>
2017-06-07 17:27:29 +0200

    * add vodafone_y_limits and sired_y_limits

--- R/constants.R
+++ R/constants.R
@@ -1,2 +1,10 @@
 province_symbols = list("Cagliari" = "CA", "Medio Campidano" = "VS", "Oristano" = "OR", "Sassari" = "SS",
                         "Nuoro" = "NU", "Olbia-Tempio" = "OT", "Ogliastra" = "OG", "Carbonia-Iglesias" = "CI")
+
+
+vodafone_y_limits = list("Cagliari" = c(0, 150000), "Medio Campidano" = c(0, 20000), "Oristano" = c(0, 50000), "Sassari" = c(0, 105000),
+                     "Nuoro" = c(0, 60000), "Olbia-Tempio" = c(0, 320000), "Ogliastra" = c(0, 40000), "Carbonia-Iglesias" = c(0,50000))
+
+
+sired_y_limits = list("Cagliari" = c(0, 25000), "Medio Campidano" = c(0, 1000), "Oristano" = c(0, 4000), "Sassari" = c(0, 15000),
+                      "Nuoro" = c(0, 10000), "Olbia-Tempio" = c(0, 45000), "Ogliastra" = c(0, 8000), "Carbonia-Iglesias" = c(0,5000))
