64bc0a972ddf0bb4a4a8f771d8413edca52b8464
pg <palla.pg@gmail.com>
2017-06-13 17:43:30 +0000

    * fix mismatching between pie slice colors and legend

--- R/outputs.R
+++ R/outputs.R
@@ -20,7 +20,7 @@ render_province_it <- function(province, input){
                 }
                 p <- plot_ly() %>%
                         add_pie(data = visitors, labels = ~origin, values = ~presence, textinfo = 'percent', text = ~paste("origin: ", origin), hoverinfo = 'text', marker = list(colors = col_it, line = list(color = '#FFFFFF')), domain = list(x = c(0, 0.5), y = c(0, 0.8))) %>%
-                        add_pie(data = sired_visitors, labels = ~regions, values = ~presence, textinfo = 'percent', text = ~paste("origin: ", regions), hoverinfo = 'text', marker = list(colors = col_it, line = list(color = '#FFFFFF')), domain = list(x = c(0.5, 1), y = c(0, 0.8))) %>%
+                        add_pie(data = sired_visitors, labels = ~regions, values = ~presence, textinfo = 'percent', text = ~paste("origin: ", regions), hoverinfo = 'text', domain = list(x = c(0.5, 1), y = c(0, 0.8))) %>%
                         layout(title =  paste("Provenienza dei visitatori italiani"), annotations = list(
                                 list(x = 0.22 , y = 0.9, text = "Dati Vodafone", showarrow = F, xref='paper', yref='paper'),
                                 list(x = 0.78 , y = 0.9, text = "Dati Sired", showarrow = F, xref='paper', yref='paper')))
